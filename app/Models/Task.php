d88ef719f27af7636bac178a246608b0867a03b5
pgonzalesmv <pgonzalesmv@gmail.com>
2025-01-08 12:06:13 -0500

    Se crea el modelo y la migracion para las tareas (task)

---
+++ app/Models/Task.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Task extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'due_date',
+        'status',
+        'user_id'
+    ];
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2025_01_08_144314_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->timestamp('due_date');
+            $table->boolean('status');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();            
+
+            // Clave foránea para la relación con la tabla users
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
